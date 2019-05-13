9825ee290e4c254601058af5dad662c147ad1fc9
Lvov Andrey <lvovandrey@mail.ru>
2019-05-13 13:17:43 +0300

    что-то не так

--- ProgressWindowAsync/MainWindow.xaml.cs
+++ ProgressWindowAsync/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,42 @@ namespace ProgressWindowAsync
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        WindowProgress windowProgress;
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Thread newWindowThread = new Thread(new ThreadStart(ThreadStartingPoint));
+            newWindowThread.SetApartmentState(ApartmentState.STA);
+            newWindowThread.IsBackground = true;
+            newWindowThread.Start();
+        }
+
+        private void ThreadStartingPoint()
+        {
+            windowProgress = new WindowProgress();
+            windowProgress.Show();
+            System.Windows.Threading.Dispatcher.Run();
+        }
+
+        private void AssignBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Assign();
+        }
+
+        void Assign()
+        {
+            windowProgress.SetVal(10);
+        }
+
+        async void AssignAsync()
+        {
+            await Task.Run(() => Assign());                         
+        }
+
+        private void AssignBtnLock_Click(object sender, RoutedEventArgs e)
+        {
+            windowProgress.SetVal(10);
         }
     }
 }
---
+++ ProgressWindowAsync/WindowProgress.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgressWindowAsync
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowProgress.xaml
+    /// </summary>
+    public partial class WindowProgress : Window
+    {
+        public WindowProgress()
+        {
+            InitializeComponent();
+        }
+        public void SetVal(int val)
+        {
+            ProgressBar1.Value = val;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetVal(90);
+        }
+    }
+}
